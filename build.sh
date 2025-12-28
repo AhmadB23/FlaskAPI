c4b316fcc00b8a0c42e0b4b595fb70a43f3382aa
AhmadB23 <70135548@student.uol.edu.pk>
2025-12-28 20:28:33 +0500

    Add database seeding to build script

--- build.sh
+++ build.sh
@@ -7,3 +7,9 @@ pip install -r requirements.txt
 
 # Run database migrations
 flask db upgrade
+
+# Seed database
+python seed_categories_authors.py
+python seed_books.py
+python seed_admin.py
+python seed_users.py
